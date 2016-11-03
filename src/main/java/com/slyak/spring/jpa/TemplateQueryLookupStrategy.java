5d2e7e3993b4928861a7d33e66e83d4fe90d9d23
stormning <stormning@163.com>
2016-11-03 09:31:02 +0800

    update jpa to latest version

--- src/main/java/com/slyak/spring/jpa/TemplateQueryLookupStrategy.java
+++ src/main/java/com/slyak/spring/jpa/TemplateQueryLookupStrategy.java
@@ -3,6 +3,7 @@ package com.slyak.spring.jpa;
 import org.springframework.data.jpa.provider.QueryExtractor;
 import org.springframework.data.jpa.repository.query.JpaQueryLookupStrategy;
 import org.springframework.data.jpa.repository.query.JpaQueryMethod;
+import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.repository.core.NamedQueries;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.query.EvaluationContextProvider;
@@ -14,35 +15,40 @@ import java.lang.reflect.Method;
 
 /**
  * .
- * <p>
+ * <p/>
  *
  * @author <a href="mailto:stormning@163.com">stormning</a>
  * @version V1.0, 2015/8/9.
  */
 public class TemplateQueryLookupStrategy implements QueryLookupStrategy {
 
-    private final EntityManager entityManager;
+	private final EntityManager entityManager;
 
-    private QueryLookupStrategy jpaQueryLookupStrategy;
+	private QueryLookupStrategy jpaQueryLookupStrategy;
 
-    private QueryExtractor extractor;
+	private QueryExtractor extractor;
 
-    public TemplateQueryLookupStrategy(EntityManager entityManager, Key key, QueryExtractor extractor, EvaluationContextProvider evaluationContextProvider) {
-        this.jpaQueryLookupStrategy = JpaQueryLookupStrategy.create(entityManager, key, extractor, evaluationContextProvider);
-        this.extractor = extractor;
-        this.entityManager = entityManager;
-    }
+	public TemplateQueryLookupStrategy(EntityManager entityManager, Key key, QueryExtractor extractor,
+			EvaluationContextProvider evaluationContextProvider) {
+		this.jpaQueryLookupStrategy = JpaQueryLookupStrategy
+				.create(entityManager, key, extractor, evaluationContextProvider);
+		this.extractor = extractor;
+		this.entityManager = entityManager;
+	}
 
-    public static QueryLookupStrategy create(EntityManager entityManager, Key key, QueryExtractor extractor, EvaluationContextProvider evaluationContextProvider) {
-        return new TemplateQueryLookupStrategy(entityManager, key, extractor, evaluationContextProvider);
-    }
+	public static QueryLookupStrategy create(EntityManager entityManager, Key key, QueryExtractor extractor,
+			EvaluationContextProvider evaluationContextProvider) {
+		return new TemplateQueryLookupStrategy(entityManager, key, extractor, evaluationContextProvider);
+	}
 
-    @Override
-    public RepositoryQuery resolveQuery(Method method, RepositoryMetadata metadata, NamedQueries namedQueries) {
-        if (method.getAnnotation(TemplateQuery.class) == null) {
-            return jpaQueryLookupStrategy.resolveQuery(method, metadata, namedQueries);
-        } else {
-            return new FreemarkerTemplateQuery(new JpaQueryMethod(method, metadata, extractor), entityManager);
-        }
-    }
+	@Override
+	public RepositoryQuery resolveQuery(Method method, RepositoryMetadata metadata, ProjectionFactory factory,
+			NamedQueries namedQueries) {
+		if (method.getAnnotation(TemplateQuery.class) == null) {
+			return jpaQueryLookupStrategy.resolveQuery(method, metadata, factory, namedQueries);
+		}
+		else {
+			return new FreemarkerTemplateQuery(new JpaQueryMethod(method, metadata, factory, extractor), entityManager);
+		}
+	}
 }
